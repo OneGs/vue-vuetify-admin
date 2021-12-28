2c4e73ad8d527e9047b060263f5b68d28cba7ede
sunzeyu <1549522434@qq.com>
2021-12-28 13:15:54 +0800

    feat(components/app-menu): 调整头部导航的信息，这是一级导航（需不需要二级的情况?）

--- src/components/app-bar/index.vue
+++ src/components/app-bar/index.vue
@@ -1,8 +1,53 @@
 <template>
-  <v-app-bar v-bind="$attrs">app-bar</v-app-bar>
+  <v-app-bar
+    v-bind="$attrs"
+    color="primary"
+    flat
+    class="lighten-1 app-bar d-flex justify-space-between"
+  >
+    <v-img
+      src="@/assets/logo.png"
+      width="46"
+      height="46"
+      contain
+      class="flex-grow-0"
+    />
+
+    <div class="flex-grow-1">menu</div>
+
+    <div class="flex-grow-0 d-flex align-center flex-row-reverse">
+      <div class="d-flex align-center">
+        <v-avatar size="36" class="lighten-1">
+          <v-img lazy-src="@/assets/user.png" :src="userAvatar" />
+        </v-avatar>
+        <span
+          class="ml-1"
+          style="
+            width: 50px;
+            display: inline-block;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          "
+          >welcome</span
+        >
+      </div>
+
+      <div class="mr-2">
+        <v-btn icon color="#fff">
+          <v-icon v-text="'mdi-magnify'" />
+        </v-btn>
+        <v-btn icon color="#fff">
+          <v-icon v-text="'mdi-bell'" />
+        </v-btn>
+        <v-btn icon color="#fff">
+          <v-icon v-text="'mdi-cog-outline'" />
+        </v-btn>
+      </div>
+    </div>
+  </v-app-bar>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
@@ -10,7 +55,21 @@ import { Component, Vue } from "vue-property-decorator";
 
   inheritAttrs: false,
 })
-export default class appBar extends Vue {}
+export default class appBar extends Vue {
+  userAvatar = "";
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.app-bar {
+  color: white;
+}
+</style>
+
+<style lang="scss">
+.app-bar {
+  .v-toolbar__content {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/views/setting-global/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Meta } from "@/libs/auto-router/src/decorators/Meta";
+
+@Meta({ title: "配置" })
+@Component({
+  name: "SettingLayout",
+})
+export default class SettingLayout extends Vue {}
+</script>
+
+<style scoped></style>
