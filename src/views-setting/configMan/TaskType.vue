6f0d0cdf1730ce5f1e679462b35e0b5a3f130167
sunzeyu <1549522434@qq.com>
2022-01-19 12:10:21 +0800

    feat(task-type): 任务类型

--- src/views-setting/configMan/TaskType.vue
+++ src/views-setting/configMan/TaskType.vue
@@ -17,27 +17,41 @@
         </v-col>
 
         <v-col class="text-right">
-          <rule-btn small>add</rule-btn>
+          <rule-dialog
+            title="Creating a Task Type"
+            @save="onSubmit"
+            ref="taskTypeDialog"
+          >
+            <rule-btn small>add</rule-btn>
+
+            <template #text>
+              <tool-auto-render :mode="taskTypeModes" />
+            </template>
+          </rule-dialog>
         </v-col>
       </v-row>
     </template>
 
     <template #item.actions="{}">
-      <rule-btn small color="default" icon="mdi-trash-can" />
+      <rule-btn small color="default" icon="mdi-trash-can" class="ml-n3" />
+      <rule-btn small color="default" icon="mdi-pencil" />
     </template>
   </tool-paginated-table>
 </template>
 
 <script lang="ts">
-import { Component, Mixins } from "vue-property-decorator";
+import { Component, Mixins, Ref } from "vue-property-decorator";
 import { Meta } from "@/libs/auto-router";
 import { getUser } from "@req/apis/base/table";
 import {
   RegisterBtn,
   RegisterCard,
+  RegisterDialog,
   RegisterForm,
   RegisterTable,
 } from "@cps/the-mixins";
+import { AutoRenderMode } from "@cps/tool-form/autoRender";
+import RuleDialog from "@cps/rule-dailog/index.vue";
 
 @Meta({ title: "Task Type", order: 100 })
 @Component({
@@ -47,21 +61,58 @@ export default class TaskType extends Mixins(
   RegisterForm,
   RegisterBtn,
   RegisterCard,
-  RegisterTable
+  RegisterTable,
+  RegisterDialog
 ) {
   items = [];
 
   taskTypeHeaders = [
     { text: "task type", value: "name", sortable: false, width: "100px" },
     { text: "comment", value: "describtion" },
-    { text: "groups", value: "sex" },
-    { text: "actions", value: "actions" },
+    { text: "groups", value: "sex", width: 100 },
+    { text: "actions", value: "actions", width: 130 },
   ];
+
+  taskTypeModes: AutoRenderMode = {
+    row: 3,
+    col: 1,
+    modes: [
+      {
+        label: "Type name",
+        position: "0-0",
+        componentName: "RuleTextField",
+        rules: "required",
+      },
+      {
+        label: "Type group",
+        position: "1-0",
+        componentName: "RuleSelect",
+        options: [
+          { label: "city", value: 1 },
+          { label: "country", value: 2 },
+          { label: "viliger", value: 3 },
+        ],
+      },
+      {
+        label: "note",
+        position: "2-0",
+        componentName: "RuleTextarea",
+      },
+    ],
+  };
+
+  @Ref() taskTypeDialog?: RuleDialog;
 
   getTaskTypes = getUser;
 
   taskTypeSearch(): void {
     console.log("enter");
+  }
+
+  onSubmit(validate: boolean): void {
+    if (!validate) return;
+
+    this.taskTypeDialog?.close();
   }
 }
 </script>
